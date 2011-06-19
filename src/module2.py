1888977ac469df1fa9d9af944aa7edcc92c328b1
Lucas <lucasp0927@gmail.com>
2011-06-19 22:17:24 +0800

    finish module2.py

--- src/module2.py
+++ src/module2.py
@@ -17,3 +17,18 @@ def df(x,y):
         out:  1D numpy array
             first derivative of y at points xout
     '''    
+    #http://www.holoborodko.com/pavel/numerical-methods/numerical-derivative/central-differences/
+    #from here 3N central differential formula is (f(x+h)-f(x-h))/2h
+    h = x[1]-x[0]
+    xout,out = [],[]
+    for i,x_i in enumerate(x[1:-1]): #first and last point dont have difference
+        xout.append(x_i)
+        out.append((y[i+2]-y[i])/2*h)#since i start frmo 0,so i need to plus 1
+    return (np.array(xout),np.array(out))
+
+if __name__ == '__main__':
+    x=[1,2,3,4,5]
+    y=[10,20,30,40,50]
+    xout,out = df(x,y)
+    print xout
+    print out
