c1332defbf631ee4f8932c001a1ae16369a14954
Lucas <lucasp0927@gmail.com>
2011-06-19 22:34:04 +0800

    finish module5.py. not test yet.

--- src/module5.py
+++ src/module5.py
@@ -14,11 +14,17 @@ def firing(t,x,x0,l,c):
             x0: float
                 point of interception
             l : float
-		distance from the motion of the particle
+                distance from the motion of the particle
             c : float
-		bullet velocity
+                bullet velocity
         OUTPUT
             t0: float
                 firing time
     '''
-        
+
+    for i,x_i in enumerate(x):
+        if (x[i]-x0)*(x0-x[i+1]) >= 0:#find faster way. multiplying may be slow?
+            inter_t = t[i]
+            break
+    # x intercept between t[i] and t[i+1]
+    return t[i]+(t[i+1]-t[i])/(x[i+1]-x[i])*(x0-x[i])
