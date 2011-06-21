5fdd34942d2406f6a081e238c33fd80c6bd087ef
Lucas <lucasp0927@gmail.com>
2011-06-21 15:34:52 +0800

    change main.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,23 @@
-from module0 import *
+#!/usr/bin/python2
+#from module0 import *
 from module1 import *
 from module2 import *
 from module3 import *
 from module4 import *
 from module5 import *
-from module6 import *
+#from module6 import *
 import matplotlib.pyplot as plt
 import numpy as np
 import time
 
 tstart=time.time()
-
-Amp, w, b, x0,v0,x_int,l,c=paraRead('input.in')
+#Amp, w, b, x0,v0,x_int,l,c=paraRead('input.in')
 # Amp : amplitude in A cos (w t)
 # w   : angular frequency
 # x0, v0: initial conditions
 # x_int : interception point
 # l : distance from the motion
 # c : velocity of the bullet
-
+for time in range(20):
+    pass   
 print 'Total time:', (time.time()-tstart)
