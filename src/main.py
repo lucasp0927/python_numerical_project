f7853eb73e5c94b6d227e3801f9c0fde17860e1d
Lucas <lucasp0927@gmail.com>
2011-06-22 08:52:01 +0800

    clean up

--- src/main.py
+++ src/main.py
@@ -22,6 +22,7 @@ c=1.0
 ############
 
 tstart=time.time()
+
 #Amp, w, b, x0,v0,x_int,l,c=paraRead('input.in')
 # Amp : amplitude in A cos (w t)
 # w   : angular frequency
@@ -29,13 +30,13 @@ tstart=time.time()
 # x_int : interception point
 # l : distance from the motion
 # c : velocity of the bullet
+
 filepath = 'data/sample'
 fit=np.zeros(4)
 #for test n=1,when in use, n should be change to n=20
 n=20
 for index in range(n):
     filename = filepath + str(index+1) + '.dat'
-    #filename="test.dat"
     tdata,xdata,vdata = dataRead(filename)
     adata=df(tdata,vdata)[1]
     P2=adata+b*vdata[1:-1]
@@ -56,12 +57,12 @@ for index in range(n):
     
 plt.show()
 fit/=float(n)
-#plt.plot(tdata,P2,tdata,fit[0]+fit[1]*tdata+fit[2]*tdata**2+fit[3]*tdata**3)
 def spring(x,t):
    x0=x[0]
    x1=x[1]
    return np.array([x1,fit[0]+fit[1]*x0+fit[2]*x0**2+fit[3]*x0**3-b*x1+Amp*np.cos(w*t)])
 #the time range is tdata[1:-1] ,x[:,0]:position,x[:,1]velocity
+
 #x=odeSolve(spring,[x0,v0],tdata)
 x=pc4(spring,[x0,v0],tdata)
 #x=rk45(spring,[x0,v0],tdata)
